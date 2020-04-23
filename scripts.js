429caedcb9f98775b8317027f51b843c2e5fbc72
nagybala <balazs.nagy1983@gmail.com>
2020-04-23 20:14:04 +0200

    update second part

--- scripts.js
+++ scripts.js
@@ -3,6 +3,9 @@ let name = 'Balu';
 console.log(name + "'s apples");
 console.log('Hello world!');
 
+$('h1').text('We are heaving a 10 minute break');
+
+
 // This a JavaScript comment
 
 /*
@@ -21,50 +24,5 @@ console.log('Hello world!');
     4
     12/4*5-2*3
     9
-    let age
-    undefined
-    age = 33
-    33
-    age*5
-    165
-    age = 12
-    12
-    age*5
-    60
-    let legs = 4
-    undefined
-    legs *2
-    8
-    let legs = 2;
-    undefined
-    legs *2
-    4
-    let anotherAge = age
-    undefined
-    anotherAge
-    12
-    let name = Balu
-    VM532:1 Uncaught ReferenceError: Balu is not defined
-        at <anonymous>:1:12
-    (anonymous) @ VM532:1
-    let name = 'Balu'
-    undefined
-    name
-    "Balu"
-    name = "Peti"
-    "Peti"
-    "Adam's apples"
-    "Adam's apples"
-    let name = 'something'
-    undefined
-    name = "Peti"
-    "Peti"
-    let name = 'Balu'
-    undefined
-    age
-    12
-    true
-    true
-    false
-    false
-    let oldEnoughToDrive = false
+ 
+   
