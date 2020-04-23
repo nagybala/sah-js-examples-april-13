54306bddf4db52f16950896222cb84c6ddcddd38
nagybala <balazs.nagy1983@gmail.com>
2020-04-23 19:10:32 +0200

    insert first part

---
+++ scripts.js
@@ -0,0 +1,70 @@
+let name = 'Balu';
+
+console.log(name + "'s apples");
+console.log('Hello world!');
+
+// This a JavaScript comment
+
+/*
+    This is a multiline
+    JavaScript comment.
+*/
+// This is a first part
+
+    12+3
+    15
+    12-5
+    7
+    13*4
+    52
+    12/3
+    4
+    12/4*5-2*3
+    9
+    let age
+    undefined
+    age = 33
+    33
+    age*5
+    165
+    age = 12
+    12
+    age*5
+    60
+    let legs = 4
+    undefined
+    legs *2
+    8
+    let legs = 2;
+    undefined
+    legs *2
+    4
+    let anotherAge = age
+    undefined
+    anotherAge
+    12
+    let name = Balu
+    VM532:1 Uncaught ReferenceError: Balu is not defined
+        at <anonymous>:1:12
+    (anonymous) @ VM532:1
+    let name = 'Balu'
+    undefined
+    name
+    "Balu"
+    name = "Peti"
+    "Peti"
+    "Adam's apples"
+    "Adam's apples"
+    let name = 'something'
+    undefined
+    name = "Peti"
+    "Peti"
+    let name = 'Balu'
+    undefined
+    age
+    12
+    true
+    true
+    false
+    false
+    let oldEnoughToDrive = false
