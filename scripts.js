2c9dd537891c7611463a117282195df1da59e917
nagybala <balazs.nagy1983@gmail.com>
2020-04-24 17:10:03 +0200

    add third part

--- scripts.js
+++ scripts.js
@@ -1,10 +1,53 @@
+// The Third part
+
 let name = 'Balu';
 
 console.log(name + "'s apples");
 console.log('Hello world!');
 
 $('h1').text('We are heaving a 10 minute break');
+$('p').html('StayAtHome Coding Camp, Likes: <strong>13</span>');
 
+$('strong').css('background', 'lime');
+$('strong').css('padding', '10px');
+
+let color = 'blue';
+
+$('h2').toggleClass(color);
+$('h2').toggleClass(color);
+
+
+$('ul').append('<li>My name is ' + name +'</li>');
+$('ul').append('<li id="special-list-item">Removable ' + name +'</li>');
+
+$('#special-list-item').remove();
+
+let addButtonHandler = () => {
+    if ($('li').length < 5){
+    $('ul').append('<li>My name is ' + name +'</li>');
+    }
+};
+
+$('#add-button').click(addButtonHandler);
+
+$('#remove-button').click(() => {
+    $('li:last-child').remove()
+});
+
+// This is the fourth part
+
+let age = 12;
+
+console.log ('Starting the if');
+console.log ('This Is a Sentence'.toLowerCase());
+
+if (age < 18) {
+  console.log ('Not old enough to drink');
+} else { 
+  console.log ('Old enough to drink');
+}
+
+console.log('This is after the if');
 
 // This a JavaScript comment
 
@@ -14,15 +57,8 @@ $('h1').text('We are heaving a 10 minute break');
 */
 // This is a first part
 
-    12+3
-    15
-    12-5
-    7
-    13*4
-    52
-    12/3
-    4
-    12/4*5-2*3
-    9
+  //  12+3
+  // 12/4*5-2*3
+  // 9
  
    
